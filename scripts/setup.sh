524b74d80bb9d4ad387744b17eb7b4865f6b622a
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-08-17 16:45:22 +0900

    udpate setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,11 @@ touch cube/.env
   echo "CUBEJS_DB_TYPE=bigquery"
   echo "CUBEJS_DB_BQ_PROJECT_ID=$(gcloud config get project)"
   echo "CUBEJS_DB_BQ_CREDENTIALS=$(cat "${HOME}/.config/gcloud/application_default_credentials.json" | base64 | tr -d '\n')"
+
+  # TODO: check if these configs are needed
+  # CUBEJS_API_SECRET=SUPERSECRET
+  # CUBEJS_EXTERNAL_DEFAULT=true
+  # CUBEJS_SCHEDULED_REFRESH_DEFAULT=true
+  # CUBEJS_DEV_MODE=true
+  # CUBEJS_SCHEMA_PATH=model
 } >> cube/.env
