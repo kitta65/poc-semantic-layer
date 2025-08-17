501ee1ae76ea0e688b4317ca8c795b22c8713df8
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-08-17 15:23:01 +0900

    minimum cube config

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+rm -rf cube/.env
+touch cube/.env
+
+{
+  echo "CUBEJS_DB_TYPE=bigquery"
+  echo "CUBEJS_DB_BQ_PROJECT_ID=$(gcloud config get project)"
+  echo "CUBEJS_DB_BQ_CREDENTIALS=$(cat "${HOME}/.config/gcloud/application_default_credentials.json" | base64 | tr -d '\n')"
+} >> cube/.env
