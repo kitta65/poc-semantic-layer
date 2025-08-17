55193dd2d7aa9ff175c9b315bae3856f36266e0c
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-08-17 18:22:55 +0900

    query success, but completion is not working in IDE

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+from schema import schema
+
 def main():
-    print("Hello from poc-semantic-layer!")
+    op = Operation(schema.query_type)
+    schema.query_type
+    cube = op.cube
+    cube.events()
+    endpoint = HTTPEndpoint("http://localhost:4000/cubejs-api/graphql")
+    data = endpoint(op)
+
+    print(data)
+
 
 
 if __name__ == "__main__":
