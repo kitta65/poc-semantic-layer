2734ad43ddb174c860bb15c615285342217ea3ec
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-08-17 17:06:32 +0900

    fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 set -euo pipefail
-cd "$(dirname "$0")"
+cd "$(dirname "$0")"/..
 
-rm -rf cube/.env
-touch cube/.env
+rm -rf .env
 
 {
-  echo "CUBEJS_DB_TYPE=bigquery"
+  echo "UID=$(id -u)"
+  echo "GID=$(id -g)"
+
   echo "CUBEJS_DB_BQ_PROJECT_ID=$(gcloud config get project)"
   echo "CUBEJS_DB_BQ_CREDENTIALS=$(cat "${HOME}/.config/gcloud/application_default_credentials.json" | base64 | tr -d '\n')"
-
-  # TODO: check if these configs are needed
-  # CUBEJS_API_SECRET=SUPERSECRET
-  # CUBEJS_EXTERNAL_DEFAULT=true
-  # CUBEJS_SCHEDULED_REFRESH_DEFAULT=true
-  # CUBEJS_DEV_MODE=true
-  # CUBEJS_SCHEMA_PATH=model
-} >> cube/.env
+} >> .env
